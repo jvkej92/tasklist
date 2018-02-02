9eedde3081f556c2e888833869f903f8befbdfe9
Jacob Johnson <loveoutjake@gmail.com>
2018-02-01 22:19:57 -0600

    Minor commenting changes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,11 +6,13 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Plain Js app with Laravel Backend</title>
-
+    <title>SPA Javascript Tasklist</title>
+    <!-- FONTS -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,300,600" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,600" rel="stylesheet" type="text/css">
+    <!-- STYLE -->
     <link href="/css/appStyle.css" rel="stylesheet" type="text/css">
+    <!-- SCRIPTS -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="/js/fontawesome-all.min.js"></script>
     <script src="/js/taskList.js"></script>
